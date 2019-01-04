4cb7a5950fbf35c3429b6f14de0604e9df5b7e38
timkit <kti80@mail.ru>
2019-01-04 23:57:09 +0600

    Add method file_w

--- src/learn9_Java7.java
+++ src/learn9_Java7.java
@@ -30,6 +30,18 @@ public class learn9_Java7 {
 		}
 
 	}
+	public static void file_w() {
+		int a[] = {212, 5, 89, 88, 48, 72 };
+		try (FileOutputStream myFile = new FileOutputStream("xyz.dat")){
+			for (int i = 0; i < a.length; i++) {
+				myFile.write(a[i]);
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Could not write file: " + e.toString());
+		}
+	}
 
 	public static void main(String[] args) {
 
